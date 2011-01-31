dcad2989f284cd2c79f914186ea3102f6227b06d
josh <devnull@localhost>
2011-02-01 00:47:18 +1100

    PME: add test script for Blue Gene

---
+++ trunk/apps/pme/test/test_bg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PARTITION=$1
+MAX_PLACES=$2
+
+i=1
+while [ $i -le 256 ]
+do
+  for j in 1 2 3
+  do
+    echo "mpirun -env X10RT_PGAS_COLLECTIVES=true -env BG_COREDUMPONEXIT=1 -env X10_NTHREADS=1 -env X10_STATIC_THREADS=true -noallocate -nofree -partition $PARTITION -mode VN -np $i bin/pme 51396"
+    mpirun -env X10RT_PGAS_COLLECTIVES=true -env BG_COREDUMPONEXIT=1 -env X10_NTHREADS=1 -env X10_STATIC_THREADS=true -noallocate -nofree -partition $PARTITION -mode VN -np $i bin/pme 51396 
+    echo ""
+  done
+  i=$(( i*2 ))
+done
