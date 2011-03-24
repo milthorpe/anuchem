cf8d65ee470e62e83b8f7e625514c6202f98ab89
josh <devnull@localhost>
2011-03-24 14:25:58 +1100

    FMM: test scripts for dcsgrad5

--- trunk/apps/fmm/test/test_bg.sh
+++ trunk/apps/fmm/test/test_bg.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+run_test() {
+  for j in 1 2 3
+  do
+    echo "$1"
+    echo "$(eval $1)"
+    echo ""
+  done
+}
+
 PARTITION=$1
 MAX_PLACES=$2
 
 i=1
 while [ $i -le 256 ]
 do
-  for j in 1 2 3
-  do
-    echo "mpirun -env X10RT_PGAS_COLLECTIVES=true -env BG_COREDUMPONEXIT=1 -env X10_NTHREADS=1 -env X10_STATIC_THREADS=true -noallocate -nofree -partition $PARTITION -mode VN -np $i bin/periodicFmm3d 51396 120"
-    mpirun -env X10RT_PGAS_COLLECTIVES=true -env BG_COREDUMPONEXIT=1 -env X10_NTHREADS=1 -env X10_STATIC_THREADS=true -noallocate -nofree -partition $PARTITION -mode VN -np $i bin/periodicFmm3d 51396 120
-    echo ""
-  done
+  run_test "mpirun -env X10RT_PGAS_COLLECTIVES=true -env BG_COREDUMPONEXIT=1 -env X10_NTHREADS=1 -env X10_STATIC_THREADS=true -noallocate -nofree -partition $PARTITION -mode VN -np $i bin/periodicFmm3d 51396 120"
   i=$(( i*2 ))
 done
