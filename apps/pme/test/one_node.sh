70ec636022c2e966cd0995d7cfa07fa3c743526d
josh <josh.milthorpe@anu.edu.au>
2011-08-12 12:17:57 +1000

    test scripts for Vayu

---
+++ apps/pme/test/one_node.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Vayu job script: test PME on one node (2x4 core Nehalem)
+#PBS -P y42 
+#PBS -q normal 
+#PBS -l walltime=01:00:00,vmem=8GB,ncpus=8
+#PBS -wd
+cat /proc/cpuinfo > cpuinfo.out
+echo "1 CPU" > test.out
+X10_NTHREADS=1 bin/pme 51396 >> test.out
+echo "\n1/8" >> test.out
+X10_NTHREADS=8 bin/pme 51396 >> test.out
+X10_NTHREADS=8 bin/pme 51396 >> test.out
+X10_NTHREADS=8 bin/pme 51396 >> test.out
+echo "\n2/4" >> test.out
+mpiexec -n 2 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+mpiexec -n 2 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+mpiexec -n 2 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+echo "\n4/2" >> test.out
+mpiexec -n 4 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
+mpiexec -n 4 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
+mpiexec -n 4 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
+echo "\n8/1" >> test.out
+mpiexec -n 8 -x X10_NTHREADS=1 -x X10_STATIC_THREADS=true bin/pme 51396 >> test.out
+mpiexec -n 8 -x X10_NTHREADS=1 -x X10_STATIC_THREADS=true bin/pme 51396 >> test.out
+mpiexec -n 8 -x X10_NTHREADS=1 -x X10_STATIC_THREADS=true bin/pme 51396 >> test.out
+
+echo "\n16 threads" >> test.out
+
+echo "\n1/16" >> test.out
+X10_NTHREADS=16 bin/pme 51396 >> test.out
+X10_NTHREADS=16 bin/pme 51396 >> test.out
+X10_NTHREADS=16 bin/pme 51396 >> test.out
+echo "\n2/8" >> test.out
+mpiexec -n 2 -x X10_NTHREADS=8 bin/pme 51396 >> test.out
+mpiexec -n 2 -x X10_NTHREADS=8 bin/pme 51396 >> test.out
+mpiexec -n 2 -x X10_NTHREADS=8 bin/pme 51396 >> test.out
+echo "\n4/4" >> test.out
+mpiexec -n 4 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+mpiexec -n 4 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+mpiexec -n 4 -x X10_NTHREADS=4 bin/pme 51396 >> test.out
+echo "\n8/2" >> test.out
+mpiexec -n 8 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
+mpiexec -n 8 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
+mpiexec -n 8 -x X10_NTHREADS=2 bin/pme 51396 >> test.out
