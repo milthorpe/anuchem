0991ed8b430de51d6c4cc96c15a5c0f8129e580f
josh <josh.milthorpe@anu.edu.au>
2011-10-01 09:53:56 +1000

    PME: test scripts on Vayu

--- apps/pme/test/vayu_scaling_single.sh
+++ apps/pme/test/vayu_scaling_single.sh
@@ -4,18 +4,14 @@
 #PBS -q normal 
 #PBS -l walltime=01:00:00,vmem=256GB,ncpus=256
 #PBS -wd
-module load openmpi
-cat /proc/cpuinfo > cpuinfo.out
+#module load ipm
 i=1
 while [ $i -le 256 ]
 do
-  echo "\n$i places"
-  for j in 1 2
-  do
-    echo "mpiexec -n $i -x X10_NTHREADS=1 -x X10_STATIC_THREADS=true bin/pme 51396"
-    mpiexec -n $i -x X10_NTHREADS=1 -x X10_STATIC_THREADS=true bin/pme 51396
-    echo ""
-  done
+  export X10_NPLACES=$i
+  export VMEM=${i}000MB
+  qsub -wd -lwalltime=00:10:00 -lncpus=$i -lvmem=$VMEM -V run_three.sh
+  echo ""
   i=$(( i*2 ))
 done
 
