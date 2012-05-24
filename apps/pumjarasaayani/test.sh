7cd8f6d05d1d0dab52150e3f1489367c00672c2f
taweetham <taweetham.limpanuparb@anu.edu.au>
2012-05-24 20:24:41 +1000

    PR:Water cluster test

---
+++ apps/pumjarasaayani/test.sh
@@ -0,0 +1,13 @@
+# don't care any shell
+runx10 bin/pumjarasaayani test/water2-cc-pVDZ.inp
+runx10 bin/pumjarasaayani test/water2-cc-pVTZ.inp
+runx10 bin/pumjarasaayani test/water2-cc-pVQZ.inp
+runx10 bin/pumjarasaayani test/water5-cc-pVDZ.inp
+runx10 bin/pumjarasaayani test/water5-cc-pVTZ.inp
+runx10 bin/pumjarasaayani test/water5-cc-pVQZ.inp
+runx10 bin/pumjarasaayani test/water10-cc-pVDZ.inp
+runx10 bin/pumjarasaayani test/water10-cc-pVTZ.inp
+runx10 bin/pumjarasaayani test/water10-cc-pVQZ.inp
+runx10 bin/pumjarasaayani test/water20-cc-pVDZ.inp
+runx10 bin/pumjarasaayani test/water20-cc-pVTZ.inp
+runx10 bin/pumjarasaayani test/water20-cc-pVQZ.inp
