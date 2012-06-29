6b20684356efc729f9563b41c2c9a9f3b0d8e510
taweetham <taweetham.limpanuparb@anu.edu.au>
2012-06-29 19:57:26 +1000

    PR:TimeStat.c

--- apps/pumjarasaayani/TimeStat.c
+++ apps/pumjarasaayani/TimeStat.c
@@ -3,11 +3,12 @@
 #include <limits.h>
 #include <float.h>
 #include <math.h>
+#include <string.h>
 
 #define MAXL (5+1)
 #define cart(l) ((l+1)*(l+2)/2)
 
-FILE *fptr; int a,b,cona,conb,n,l; double time,ntime,fwcount;
+FILE *fptr; int a,b,cona,conb,n,l; double time,ntime,fwcount; char fname[256];
 
 double min[MAXL][MAXL],Min,
        max[MAXL][MAXL],Max,
@@ -19,14 +20,12 @@ long count[MAXL][MAXL],Count;
 int F123[MAXL][MAXL],F4[MAXL][MAXL],
     W123[MAXL][MAXL],W4[MAXL][MAXL];
 
-void clear(){
-    Min=DBL_MAX; Max=DBL_MIN;
-    S=0.; SS=0.;
-    Count=0;
+void init(){
+    if (!(fptr=fopen(fname,"r")))  {printf("Can not open '%s'.\n",fname); exit(1);} else printf("Reading '%s'...\n",fname);
+    Min=DBL_MAX; Max=DBL_MIN; S=0.; SS=0.; Count=0;
     for (a=0; a<MAXL; a++) for (b=0; b<=a; b++) {
         min[a][b]=DBL_MAX; max[a][b]=DBL_MIN;
-        s[a][b]=0.; ss[a][b]=0.;
-        count[a][b]=0;
+        s[a][b]=0.; ss[a][b]=0.; count[a][b]=0;
     }
 }
 
@@ -38,11 +37,11 @@ void tally(){
     count[a][b]++; Count++;
 }
 
-void printstat(){
+void printstat(char *title){
+    printf("%s\n",title);
     printf(" a  b       n          min         mean         s.d.          max\n-----------------------------------------------------------------\n");
     for (a=0; a<MAXL; a++) for (b=0; b<=a; b++) {
-        mean=s[a][b]/count[a][b];
-        sd=sqrt(ss[a][b]/count[a][b]-mean*mean);
+        mean=s[a][b]/count[a][b]; sd=sqrt(ss[a][b]/count[a][b]-mean*mean);
         if (count[a][b]) printf("%2d %2d %7ld %e %e %e %e\n",a,b,count[a][b],min[a][b],mean,sd,max[a][b]);
     }
     mean=S/Count; sd=sqrt(SS/Count-mean*mean);
@@ -50,7 +49,7 @@ void printstat(){
 }
 
 int main(int argc, char *argv[]) {
-    if (argc<2) {printf("Please specify output file.\n"); exit(1);}
+    if (argc<2) {printf("Please specify output file.\n"); exit(1);} else strcpy(fname,argv[1]);
 
     // READ FETCH/WRITE COST PARAMETERS
     fptr=fopen("cost.param","r");
@@ -63,8 +62,7 @@ int main(int argc, char *argv[]) {
     fclose(fptr);
 
     // TALLY TIME PER FETCH & WRITE
-    if (!(fptr=fopen(argv[1],"r")))  {printf("Can not open '%s'.\n",argv[1]); exit(1);} else printf("Reading '%s'...\n",argv[1]);
-    clear();
+    init();
     while (!feof(fptr)) {
         fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
         fwcount=pow(l+1.,2.)*((F123[a][b]+W123[a][b])*cona*conb+F4[a][b]+W4[a][b]);
@@ -72,31 +70,25 @@ int main(int argc, char *argv[]) {
         tally();     
     }
     fclose(fptr);
-    printf("Timing statistics per FETCH/WRITE\n");
-    printstat();
+    printstat("Timing statistics per FETCH/WRITE");
 
     // TALLY TIME PER CONTRACTED INTEGRAL
-    if (!(fptr=fopen(argv[1],"r")))  {printf("Can not open '%s'.\n",argv[1]); exit(1);} else printf("Reading '%s'...\n",argv[1]);
-    clear();
+    init();
     while (!feof(fptr)) {
         fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
         ntime=time/pow(l+1.,2.)/cart(a)/cart(b);
         tally();
     }
     fclose(fptr); 
-    printf("Timing statistics per FETCH/WRITE\n");
-    printstat();
+    printstat("Timing statistics per CONTRACTED INTEGRAL");
 
     // TALLY TIME PER GENCLASS CALL
-    if (!(fptr=fopen(argv[1],"r")))  {printf("Can not open '%s'.\n",argv[1]); exit(1);} else printf("Reading '%s'...\n",argv[1]);
-    clear();
+    init();
     while (!feof(fptr)) {
         fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
         ntime=time;
         tally();
     }
     fclose(fptr);
-    printf("Timing statistics per FETCH/WRITE\n");
-    printstat();
-
+    printstat("Timing statistics per INTPACK CALL");
 }
