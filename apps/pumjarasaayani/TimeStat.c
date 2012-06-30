e6261f6a9305280f179ff91b2aeac807a3157068
taweetham <taweetham.limpanuparb@anu.edu.au>
2012-06-30 17:46:51 +1000

    PR:TimeStat.c

--- apps/pumjarasaayani/TimeStat.c
+++ apps/pumjarasaayani/TimeStat.c
@@ -7,15 +7,14 @@
 
 #define MAXL (5+1)
 #define cart(l) ((l+1)*(l+2)/2)
+#define read fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
 
 FILE *fptr; int a,b,cona,conb,n,l; double time,ntime,fwcount; char fname[256];
-
 double min[MAXL][MAXL],Min,
        max[MAXL][MAXL],Max,
        s[MAXL][MAXL],S,
        ss[MAXL][MAXL],SS,
        mean,sd;
-
 long count[MAXL][MAXL],Count;
 int F123[MAXL][MAXL],F4[MAXL][MAXL],
     W123[MAXL][MAXL],W4[MAXL][MAXL];
@@ -38,6 +37,7 @@ void tally(){
 }
 
 void printstat(char *title){
+    fclose(fptr);
     printf("%s\n",title);
     printf(" a  b       n          min         mean         s.d.          max\n-----------------------------------------------------------------\n");
     for (a=0; a<MAXL; a++) for (b=0; b<=a; b++) {
@@ -64,31 +64,28 @@ int main(int argc, char *argv[]) {
     // TALLY TIME PER FETCH & WRITE
     init();
     while (!feof(fptr)) {
-        fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
+        read;
         fwcount=pow(l+1.,2.)*((F123[a][b]+W123[a][b])*cona*conb+F4[a][b]+W4[a][b]);
         ntime=time/fwcount;   
         tally();     
     }
-    fclose(fptr);
     printstat("Timing statistics per FETCH/WRITE");
 
     // TALLY TIME PER CONTRACTED INTEGRAL
     init();
     while (!feof(fptr)) {
-        fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
+        read;
         ntime=time/pow(l+1.,2.)/cart(a)/cart(b);
         tally();
     }
-    fclose(fptr); 
     printstat("Timing statistics per CONTRACTED INTEGRAL");
 
     // TALLY TIME PER GENCLASS CALL
     init();
     while (!feof(fptr)) {
-        fscanf(fptr,"%d %d %d %d %d %d %lf",&a,&b,&cona,&conb,&n,&l,&time);
+        read;
         ntime=time;
         tally();
     }
-    fclose(fptr);
     printstat("Timing statistics per INTPACK CALL");
 }
