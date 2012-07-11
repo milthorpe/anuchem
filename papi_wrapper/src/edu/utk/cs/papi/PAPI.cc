e825a39dbff0d27a9dbf33c1f3898222a3b5506e
josh <josh.milthorpe@anu.edu.au>
2012-07-11 22:00:20 +1000

    PAPI wrapper: added load/store miss events

--- papi_wrapper/src/edu/utk/cs/papi/PAPI.cc
+++ papi_wrapper/src/edu/utk/cs/papi/PAPI.cc
@@ -57,11 +57,13 @@ namespace edu {
                     addEvent(PAPI_TOT_INS);
                     addEvent(PAPI_LD_INS);
                     addEvent(PAPI_SR_INS);
+                    addEvent(PAPI_L1_LDM);
+                    addEvent(PAPI_L1_STM);
                     resetTotals();
                 }
 
                 void PAPI::printMemoryOps() {
-                    printf("total cyc: %16lld total ins: %16lld total LD ins: %16lld total SR ins: %16lld\n", cycles, totals[0], totals[1], totals[2]);
+                    printf("cycles: %16lld total ins: %16lld LD ins: %16lld SR ins: %16lld L1 LD miss: %16lld L1 ST miss: %16lld\n", cycles, totals[0], totals[1], totals[2], totals[3], totals[4]);
                 }
 
                 void PAPI::startCount() {
