6b4d66a85d378f2fff655e5e1c06db0ec0e2382d
taweetham <taweetham.limpanuparb@anu.edu.au>
2012-11-29 17:44:32 +1100

    GotoBLAS

--- apps/pumjarasaayani/src/au/edu/anu/qm/TestDGEMM.c
+++ apps/pumjarasaayani/src/au/edu/anu/qm/TestDGEMM.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
    int i,j;
 
    double (*K)[N],(*auxIntMat)[N],(*halfAuxMat)[N*roK],(*mos)[N];
-   
+
    K = (double (*)[N]) malloc(sizeof(double)*N*N);
    auxIntMat = (double (*)[N]) malloc(sizeof(double)*N*N*roK);
    halfAuxMat = (double (*)[N*roK]) malloc(sizeof(double)*N*nOrbital*roK);
@@ -18,11 +18,11 @@ int main(int argc, char* argv[]) {
    for (i=0; i<N; i++) for (j=0; j<N; j++)
        K[i][j]=0.;
 
-   for (i=0; i<N*roK; i++) for (j=0; j<N; j++) 
+   for (i=0; i<N*roK; i++) for (j=0; j<N; j++)
        auxIntMat[i][j]=(double)i+j;
 
 // for (i=0; i<nOrbital; i++)  for (j=0; j<N*roK; j++)
-//     halfAuxMat[i][j]=(double)i+j; 
+//     halfAuxMat[i][j]=(double)i+j;
 
    for (i=0; i<nOrbital; i++) for (j=0; j<N; j++)
        mos[i][j]=(double)i+j;
@@ -32,10 +32,20 @@ int main(int argc, char* argv[]) {
    double alpha,beta;
    int LDA,LDB,LDC,m,n,k;
 
-   printf("mkl_get_max_threads()=%d\n",mkl_get_max_threads());
-   //mkl_set_num_threads(1); segmetation fault http://software.intel.com/en-us/forums/topic/299365
-   MKL_Set_Num_Threads(8);
-   printf("mkl_get_max_threads()=%d\n",mkl_get_max_threads());
+   // printf("mkl_get_max_threads()=%d\n",mkl_get_max_threads());
+   // mkl_set_num_threads(1); segmetation fault http://software.intel.com/en-us/forums/topic/299365
+   // MKL_Set_Num_Threads(8);
+   // printf("mkl_get_max_threads()=%d\n",mkl_get_max_threads());
+
+for (i=1; i<=4; i++) {
+
+   printf("blas_get_cpu_number()=%d\n",blas_get_cpu_number());
+   printf("get_num_nodes()=%d\n",get_num_nodes());
+   //printf("omp_get_num_procs()=%d\n",omp_get_num_procs());
+   goto_set_num_threads(i);
+   printf("blas_get_cpu_number()=%d\n",blas_get_cpu_number());
+   printf("get_num_nodes()=%d\n",get_num_nodes());
+
 
    // Because of Fortran vs C convention DGEMM call will result in C = B x A
    // The LDx variables should be # Columns of corresponding matrix
@@ -54,7 +64,7 @@ int main(int argc, char* argv[]) {
    start=clock();
    dgemm_("N", "T", &m, &n, &k, &alpha, halfAuxMat2, &LDA, halfAuxMat2, &LDB, &beta, K, &LDC);
    printf("2nd DGEMM completed! %f s\n",(double)(clock()-start)/CLOCKS_PER_SEC);
-
+}
    return 0;
 }
 
