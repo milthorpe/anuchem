7678da0065cc26266f6425885ac0622b77852230
josh <josh.milthorpe@anu.edu.au>
2013-08-18 12:09:58 +1000

    PR Vayu test script: perhost

--- apps/pumjarasaayani/test/run_intel_mpi.sh
+++ apps/pumjarasaayani/test/run_intel_mpi.sh
@@ -2,6 +2,6 @@ module unload openmpi
 module load intel-mpi
 for iter in {1..10}
 do
-  echo "mpirun -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water5-cc-pVQZ.inp"
-  mpirun -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water5-cc-pVQZ.inp
+  echo "mpirun -perhost 2 -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water5-cc-pVQZ.inp"
+  mpirun -perhost 2 -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water5-cc-pVQZ.inp
 done
