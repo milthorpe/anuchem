01086b155819bf1ca4e0b5470a15750d1e39e6fe
josh <josh.milthorpe@anu.edu.au>
2013-11-22 10:51:12 +1100

    PR: single-thread tests

--- apps/pumjarasaayani/test/raijin_single.sh
+++ apps/pumjarasaayani/test/raijin_single.sh
@@ -2,13 +2,27 @@
 # Raijin job script: single socket (1 place per 8 core Sandy Bridge)
 . ~/x10-trunk/x10.profile
 
-test[0]=1D-tetrapep-6311g
-test[1]=1D-tetrapep-cc-pVDZ
-test[2]=1D-tetrapep-cc-pVTZ
-test[3]=3D-tetrapep-cc-pVDZ
-test[4]=3D-tetrapep-cc-pVTZ
+test[0]=water5-cc-pVDZ
+test[1]=water5-cc-pVTZ
+test[2]=water5-cc-pVQZ
+test[3]=water10-cc-pVDZ
+test[4]=water10-cc-pVTZ
+test[5]=water10-cc-pVQZ
+test[6]=1D-tetrapep-6311g
+test[7]=1D-tetrapep-cc-pVDZ
+test[8]=1D-tetrapep-cc-pVTZ
+test[9]=1D-tetrapep-cc-pVQZ
+test[10]=3D-tetrapep-6311g
+test[11]=3D-tetrapep-cc-pVDZ
+test[12]=3D-tetrapep-cc-pVTZ
+test[13]=3D-tetrapep-cc-pVQZ
+test[14]=1D-octapep-6311g
+test[15]=3D-octapep-6311g
+test[16]=3D-octapep-cc-pVDZ
+test[17]=3D-octapep-cc-pVTZ
+test[18]=3D-octapep-cc-pVQZ
 
-for i in {0..4}
+for i in {0..18}
 do
     export TESTFILE=test/${test[i]}.inp
 
