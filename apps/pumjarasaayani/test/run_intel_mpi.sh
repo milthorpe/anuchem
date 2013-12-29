5397f7124afe809678c1c888c02fa41a7778c1e6
josh <josh.milthorpe@anu.edu.au>
2013-12-29 14:39:01 +1100

    PR: test molecule specification through TESTFILE

--- apps/pumjarasaayani/test/run_intel_mpi.sh
+++ apps/pumjarasaayani/test/run_intel_mpi.sh
@@ -3,8 +3,9 @@ module unload openmpi
 module load intel-mpi
 export HOSTS=${PBS_JOBID}_hosts.txt
 uniq < $PBS_NODEFILE > $HOSTS
+export TESTFILE=test/3D-tetrapep-cc-pVQZ.inp
 for iter in {1..10}
 do
-  echo "mpirun -binding domain=socket -perhost 2 -hostfile $HOSTS -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water10-cc-pVQZ.inp"
-  mpirun -binding domain=socket -perhost 2 -hostfile $HOSTS -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani test/water10-cc-pVQZ.inp
+  echo "mpirun -binding domain=socket -perhost 2 -hostfile $HOSTS -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani $TESTFILE"
+  mpirun -binding domain=socket -perhost 2 -hostfile $HOSTS -n $X10_NPLACES -env X10_NTHREADS $X10_NTHREADS -env OMP_NUM_THREADS $X10_NTHREADS bin/pumjarasaayani $TESTFILE
 done
--- apps/pumjarasaayani/test/run_ten.sh
+++ apps/pumjarasaayani/test/run_ten.sh
@@ -1,5 +1,7 @@
+. ~/x10-trunk/x10.profile
+export TESTFILE=test/3D-tetrapep-cc-pVQZ.inp
 for iter in {1..10}
 do
-  echo "X10_NTHREADS=$X10_NTHREADS bin/pumjarasaayani test/water5-cc-pVQZ.inp"
-  bin/pumjarasaayani test/water5-cc-pVQZ.inp
+  echo "X10_NTHREADS=$X10_NTHREADS bin/pumjarasaayani $TESTFILE"
+  bin/pumjarasaayani $TESTFILE
 done
