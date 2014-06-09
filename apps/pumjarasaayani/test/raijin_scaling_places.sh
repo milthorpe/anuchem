931efbfc85965852bbe7ed049e41375081845069
josh <josh.milthorpe@gmail.com>
2014-06-09 09:35:18 -0400

    PR test scripts: make cores, memory request clear

--- apps/pumjarasaayani/test/raijin_scaling_places.sh
+++ apps/pumjarasaayani/test/raijin_scaling_places.sh
@@ -2,14 +2,14 @@
 # Raijin job script: test PR scaling to 8 places (1 place per 8 core Sandy Bridge)
 . ~/x10-trunk/x10.profile
 i=1
-while [ $i -le 16 ]
+while [ $i -le 128 ]
 do
   export X10_NPLACES=$i
   export X10_NTHREADS=8
-  export NCPUS=$((X10_NPLACES*8))
-  MEM=$((NCPUS*2))GB # could be 3GB/CPU, but go easy
-  echo $NCPUS $MEM
-  qsub -l wd -lwalltime=00:15:00 -lncpus=$NCPUS -l mem=$MEM -V test/run_intel_mpi.sh
+  export NCORES=$((X10_NPLACES*8))
+  MEM=$((NCORES*2))GB 
+  echo $NCORES $MEM
+  qsub -l wd -lwalltime=00:45:00 -lncpus=$NCORES -l mem=$MEM -V test/run_intel_mpi.sh
   echo ""
   i=$(( i*2 ))
 done
--- apps/pumjarasaayani/test/raijin_scaling_threads.sh
+++ apps/pumjarasaayani/test/raijin_scaling_threads.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Raijin test script: test strong scaling with threads on a single 16-core node
-#module load ipm
 #module load hpctoolkit
 i=1
 while [ $i -le 18 ]
 do
   export X10_NPLACES=1
   export X10_NTHREADS=$i
-  export NCPUS=$((X10_NPLACES*16))
-  MEM=$((NCPUS*2))GB
-  echo $NCPUS $MEM
-  qsub -l wd -lwalltime=00:15:00 -lncpus=$NCPUS -l mem=$MEM -V test/run_ten.sh
+  export NCORES=$((X10_NPLACES*16))
+  MEM=$((NCORES*2))GB
+  echo $NCORES $MEM
+  qsub -l wd -lwalltime=00:45:00 -lncpus=$NCORES -l mem=$MEM -V test/run_ten.sh
   echo ""
   i=$(( i+1 ))
 done
