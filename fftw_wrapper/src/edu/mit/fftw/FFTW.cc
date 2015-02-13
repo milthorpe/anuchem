173f96aa2abd68fc1f0eb2baac0de264433f4489
josh <josh.milthorpe@gmail.com>
2015-02-12 23:19:21 -0500

    fftw-wrapper: cleanup C++ code; copyright

--- fftw_wrapper/src/edu/mit/fftw/FFTW.cc
+++ fftw_wrapper/src/edu/mit/fftw/FFTW.cc
@@ -1,85 +1,95 @@
-#include "FFTW.h"
+/*
+ * This file is part of ANUChem.
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * (C) Copyright Josh Milthorpe 2010-2013.
+ */
+
 #include <fftw3.h>
+#include "FFTW.h"
 
 namespace edu {
-    namespace mit {
-        namespace fftw {
+namespace mit {
+namespace fftw {
 
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, fftw_complex* input, fftw_complex* output, bool forward) {
-                return fftw_plan_dft_1d(N, input, output, forward?-1:1, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, double* input, fftw_complex* output) {
-                return fftw_plan_dft_r2c_1d(N, input, output, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, fftw_complex* input, double* output) {
-                return fftw_plan_dft_c2r_1d(N, input, output, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, fftw_complex* input, fftw_complex* output, bool forward) {
-                return fftw_plan_many_dft(1, &N, howmany,
-                                      input, &N,
-                                      1, N,
-                                      output, &N,
-                                      1, N,
-                                      forward?-1:1, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, fftw_complex* input, double* output) {
-                return fftw_plan_many_dft_c2r(1, &N, howmany,
-                                      input, &N,
-                                      1, N,
-                                      output, &N,
-                                      1, N,
-                                      FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, double* input, fftw_complex* output) {
-                return fftw_plan_many_dft_r2c(1, &N, howmany,
-                                      input, &N,
-                                      1, N,
-                                      output, &N,
-                                      1, N,
-                                      FFTW_ESTIMATE);
-            }
-            
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, fftw_complex* output, bool forward) {
-                return fftw_plan_dft_3d(N1, N2, N3, input, output, forward?-1:1, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, double* input, fftw_complex* output) {
-                return fftw_plan_dft_r2c_3d(N1, N2, N3, input, output, FFTW_ESTIMATE);
-            }
-
-            FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, double* output) {
-                return fftw_plan_dft_c2r_3d(N1, N2, N3, input, output, FFTW_ESTIMATE);
-            }
-
-            void FFTWWrapper::fftwExecute(FFTW_FFTWPlan p) {
-                fftw_execute(p);
-            }
-
-            void FFTWWrapper::fftwDestroyPlan(FFTW_FFTWPlan p) {
-                fftw_destroy_plan(p);
-            }
-
-            // TODO threaded FFTW.  This is not needed ATM.
-            /*
-            int FFTWWrapper::fftwInitThreads() {
-                return fftw_init_threads();
-            }
-
-            void FFTWWrapper::fftwPlanWithNThreads(int nThreads) {
-                fftw_plan_with_nthreads(nThreads);
-            }
-
-            void FFTWWrapper::fftwCleanupThreads() {
-                fftw_cleanup_threads();
-            }
-            */
-        }
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, fftw_complex* input, fftw_complex* output, bool forward) {
+        return fftw_plan_dft_1d(N, input, output, forward?-1:1, FFTW_ESTIMATE);
     }
-}
 
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, double* input, fftw_complex* output) {
+        return fftw_plan_dft_r2c_1d(N, input, output, FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, fftw_complex* input, double* output) {
+        return fftw_plan_dft_c2r_1d(N, input, output, FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, fftw_complex* input, fftw_complex* output, bool forward) {
+        return fftw_plan_many_dft(1, &N, howmany,
+                              input, &N,
+                              1, N,
+                              output, &N,
+                              1, N,
+                              forward?-1:1, FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, fftw_complex* input, double* output) {
+        return fftw_plan_many_dft_c2r(1, &N, howmany,
+                              input, &N,
+                              1, N,
+                              output, &N,
+                              1, N,
+                              FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft1d(int N, int howmany, double* input, fftw_complex* output) {
+        return fftw_plan_many_dft_r2c(1, &N, howmany,
+                              input, &N,
+                              1, N,
+                              output, &N,
+                              1, N,
+                              FFTW_ESTIMATE);
+    }
+ 
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, fftw_complex* output, bool forward) {
+        return fftw_plan_dft_3d(N1, N2, N3, input, output, forward?-1:1, FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, double* input, fftw_complex* output) {
+        return fftw_plan_dft_r2c_3d(N1, N2, N3, input, output, FFTW_ESTIMATE);
+    }
+
+    FFTW_FFTWPlan FFTWWrapper::fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, double* output) {
+        return fftw_plan_dft_c2r_3d(N1, N2, N3, input, output, FFTW_ESTIMATE);
+    }
+
+    void FFTWWrapper::fftwExecute(FFTW_FFTWPlan p) {
+        fftw_execute(p);
+    }
+
+    void FFTWWrapper::fftwDestroyPlan(FFTW_FFTWPlan p) {
+        fftw_destroy_plan(p);
+    }
+
+    // TODO threaded FFTW.  This is not needed ATM.
+    /*
+    int FFTWWrapper::fftwInitThreads() {
+        return fftw_init_threads();
+    }
+
+    void FFTWWrapper::fftwPlanWithNThreads(int nThreads) {
+        fftw_plan_with_nthreads(nThreads);
+    }
+
+    void FFTWWrapper::fftwCleanupThreads() {
+        fftw_cleanup_threads();
+    }
+    */
+}  // namespace fftw
+}  // namespace mit
+}  // namespace edu
 
--- fftw_wrapper/src/edu/mit/fftw/FFTW.h
+++ fftw_wrapper/src/edu/mit/fftw/FFTW.h
@@ -1,33 +1,42 @@
+/*
+ * This file is part of ANUChem.
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * (C) Copyright Josh Milthorpe 2010-2013.
+ */
+
 #ifndef EDU_MIT_FFTW_FFTW_H
 #define EDU_MIT_FFTW_FFTW_H
 
 #include <fftw3.h>
 
 namespace edu {
-    namespace mit {
-        namespace fftw {
-            typedef fftw_plan FFTW_FFTWPlan;
+namespace mit {
+namespace fftw {
+typedef fftw_plan FFTW_FFTWPlan;
 
-            class FFTWWrapper {
-                public:
-                    static int fftwInitThreads();
-                    static void fftwPlanWithNThreads(int nthreads);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int, fftw_complex*, fftw_complex*, bool);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int, double*, fftw_complex*);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int, fftw_complex*, double*);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, fftw_complex* input, fftw_complex* output, bool forward);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, double* input, fftw_complex* output);
-                    static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, fftw_complex* input, double* output);
-                    static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, fftw_complex* output, bool forward);
-                    static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, double* output);
-                    static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, double* input, fftw_complex* output);
-                    static void fftwExecute(FFTW_FFTWPlan);
-                    static void fftwDestroyPlan(FFTW_FFTWPlan);
-                    static void fftwCleanupThreads();
-            };
-        }
-    }
-}
+class FFTWWrapper {
+ public:
+        static int fftwInitThreads();
+        static void fftwPlanWithNThreads(int nthreads);
+        static FFTW_FFTWPlan fftwPlanDft1d(int, fftw_complex*, fftw_complex*, bool);
+        static FFTW_FFTWPlan fftwPlanDft1d(int, double*, fftw_complex*);
+        static FFTW_FFTWPlan fftwPlanDft1d(int, fftw_complex*, double*);
+        static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, fftw_complex* input, fftw_complex* output, bool forward);
+        static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, double* input, fftw_complex* output);
+        static FFTW_FFTWPlan fftwPlanDft1d(int N, int howmany, fftw_complex* input, double* output);
+        static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, fftw_complex* output, bool forward);
+        static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, fftw_complex* input, double* output);
+        static FFTW_FFTWPlan fftwPlanDft3d(int N1, int N2, int N3, double* input, fftw_complex* output);
+        static void fftwExecute(FFTW_FFTWPlan);
+        static void fftwDestroyPlan(FFTW_FFTWPlan);
+        static void fftwCleanupThreads();
+};
+}  // namespace fftw
+}  // namespace mit
+}  // namespace edu
 #endif
-
-
